2b0eb78b85bed1dfc78a83eb7636374a13ba265c
Efim Aniskin <efimaniskin@gmail.com>
2025-04-10 12:28:09 +0300

    complete data_base

---
+++ app/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: str
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+
+    @property
+    def DATABASE_URL_ASYNC(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
--- app/models.py
+++ app/models.py
@@ -1,10 +1,52 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from datetime import datetime
+from enum import Enum
+from sqlalchemy import Enum as SQLEnam, ForeignKey, Integer, Text, func
+from sqlalchemy.dialects.postgresql import ARRAY, TIMESTAMP
+
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column
 
 Base = declarative_base()
+
+
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    email = Column(String, unique=True)
-    password = Column(String)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+
+
+class ChatType(Enum):
+    PRIVATE = "private"
+    GROUP = "group"
+
+
+class Chat(Base):
+    __tablename__ = "chats"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    type: Mapped[ChatType] = mapped_column(SQLEnam(ChatType, name="chat_type"), nullable=False)
+
+
+"""
+Я бы сделал через m2m но по заданию структура базы данных должна быть именно такой
+"""
+class Group(Base):
+    __tablename__ = "groups"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    creator_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    participants: Mapped[list[int]] = mapped_column(ARRAY(Integer), default=list)
+
+class ReadStatus(Enum):
+    READ = "read"
+    UNREAD = "unread"
+
+class Message(Base):
+    __tablename__ = "messages"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    chat_id: Mapped[int] = mapped_column(ForeignKey("chats.id"))
+    sender_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    text: Mapped[str] = mapped_column(Text)
+    timestamp: Mapped[datetime] = mapped_column(TIMESTAMP, server_default=func.now(), nullable=False)
+    read_status: Mapped[ReadStatus] = mapped_column(SQLEnam(ReadStatus, name="read_status"), default=ReadStatus.UNREAD)
